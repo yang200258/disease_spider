f39ee71d58ac208a1e6eaf945e65c870df649bb8
杨青青4 <qqing_yang@eking-tech.com>
2020-05-28 16:05:50 +0800

    final

--- cdc_spider.py
+++ cdc_spider.py
@@ -9,6 +9,10 @@ import js2py
 from apscheduler.schedulers.background import BlockingScheduler
 from scrapy import Selector
 import requests
+from email.mime.text import MIMEText
+# # import smtplib
+# from smtplib import SMTP_SSL
+# from email.header import Header
 
 from cdc_models_formal import t_article as t_article_test, t_category as t_category_test, \
     t_article_category as middel_test
@@ -27,7 +31,9 @@ headers_health = {
 }
 organization_dict = {'传染病': '急性传染病预防控制室', '突发公共卫生事件': '卫生应急管理室', '慢性非传染性疾病': '地方病与慢性病防治室', '营养与健康': '营养与学生健康检测室',
                      '环境与健康': '环境卫生室', '职业卫生与中毒控制': '职业卫生室', '辐射防护': '辐射安全室'}
-
+from_addr = "502258389@qq.com"
+to_addr = "2581177069@qq.com"
+password = "rrzbldfkcfbkcbea"
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
@@ -38,9 +44,10 @@ scheduler = BlockingScheduler()
 
 def my_listener(event):
     if event.exception:
-        print('任务出现异常！')
+        # sendMail('爬取疾控中心任务运行失败，请检查原因！')
+        logging.info('任务出现异常！')
     else:
-        print('任务照常运行...')
+        logging.info('任务照常运行...')
 
 
 scheduler.add_listener(my_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
@@ -121,7 +128,7 @@ def get_health_urls(url):
         next_page_url = health_sel.xpath("//ul[@class='pagination']/li/a/@href").extract()[-1]
         get_health_urls(next_page_url)
     except Exception as e:
-        print("获取健康知识url完成")
+        logging.info("获取健康知识url完成")
         return
 
 
@@ -163,8 +170,6 @@ def get_topic(url):
     else:
         # 一级分类名称
         disease_category = next_page_sel.xpath("//div[@class='cn-title']/p/text()").extract()[0]
-        # 保存一级分类
-        save_category(disease_category, '')
         topic = next_page_sel.xpath("//h2[@class='method-item-title']/a/text()").extract()[0]
         if topic == '中毒有关知识':
             next_page_topic = next_page_sel.xpath("//h2[@class='method-item-title']/a/text()").extract()[1]
@@ -173,6 +178,8 @@ def get_topic(url):
             next_page_topic = next_page_sel.xpath("//h2[@class='method-item-title']/a/text()").extract()[0]
             next_page_url = next_page_sel.xpath("//h2[@class='method-item-title']/a/@href").extract()[0]
         if disease_category not in unspider_title:
+            # 保存一级分类
+            save_category(disease_category, '')
             if next_page_topic == '知识天地':
                 # 处理不带有二级分类的文章
                 article_url = parse.urljoin(next_url, next_page_url)
@@ -382,21 +389,23 @@ def save_article(title, createtime, category_name, content, note, source):
     try:
         exist_article_test = t_article_test.select().where(t_article_test.note == article.note)
         if exist_article_test:
-            logging.info("url为 {} 的文章已存在，不做操作！".format(note))
+            pass
+            # logging.info("url为 {} 的文章已存在，不做操作！".format(note))
         else:
             article.save(force_insert=True)
             m_test.save(force_insert=True)
     except Exception as e:
-        print(e)
+        pass
     try:
         exist_article_formal = t_article_formal.select().where(t_article_formal.note == article.note)
         if exist_article_formal:
-            logging.info("formal中url为 {} 的文章已存在，不做操作！".format(note))
+            pass
+            # logging.info("formal中url为 {} 的文章已存在，不做操作！".format(note))
         else:
             article_formal.save(force_insert=True)
             m_formal.save(force_insert=True)
     except Exception as e:
-        print(e)
+        pass
 
 
 def save_category(category_name, parent_name):
@@ -430,52 +439,87 @@ def save_category(category_name, parent_name):
     exist_category = t_category_test.select().where(t_category_test.category_name == category.category_name)
     exist_category_formal = t_category_formal.select().where(t_category_formal.category_name == category.category_name)
     if exist_category:
-        logging.info("名称为 {} 的类别已存在，不做操作！".format(category_name))
+        pass
+        # logging.info("名称为 {} 的类别已存在，不做操作！".format(category_name))
     else:
         category.save(force_insert=True)
     if exist_category_formal:
-        logging.info("formal中名称为 {} 的类别已存在，不做操作！".format(category_name))
+        pass
+        # logging.info("formal中名称为 {} 的类别已存在，不做操作！".format(category_name))
     else:
         category_formal.save(force_insert=True)
 
 
 def get_health(urls):
-    print("开始爬取健康知识！", len(urls))
+    logging.info("开始爬取健康知识!")
     for url in urls:
         # get_health_article(url)
         task = executor.submit(get_health_article, url)
         wait([task])
-    print("爬取健康知识完成！")
+    logging.info("爬取健康知识完成！")
 
 
 def get_cdc(urls):
-    print("开始爬取疾控知识！")
+    logging.info("开始爬取疾控知识！")
     for item in urls:
         # get_topic(item)
         # 启动多线程 **********************
         task = executor.submit(get_topic, item)
         wait([task])
-    print("爬取疾控知识完成！")
+    logging.info("爬取疾控知识完成！")
+
+
+# def sendMail(mail_content):
+#     # mail_content = 'This is a content of the mail'
+#     try:
+#         content = MIMEText(mail_content, 'plain',
+#                            'utf-8')  # 第一个参数：邮件的内容；第二个参数：邮件内容的格式，普通的文本，可以使用:plain,如果想使内容美观，可以使用:html；第三个参数：设置内容的编码，这里设置为:utf-8
+#         reveivers = Header(to_addr)
+#         content['To'] = reveivers  # 设置邮件的接收者，多个接收者之间用逗号隔开
+#         content['From'] = Header(from_addr)  # 邮件的发送者,最好写成str("这里填发送者")，不然可能会出现乱码
+#         content['Subject'] = Header("爬虫每日结果推送")  # 邮件的主题
+#
+#         ##############使用qq邮箱的时候，记得要去开启你的qq邮箱的smtp服务；##############
+#         # 方法：
+#         # 1）登录到你的qq邮箱；
+#         # 2）找到首页顶部的【设置】并点击；
+#         # 3）找到【账户】这个选项卡并点击，然后在页面中找到“SMTP”相关字样，找到【开启】的超链接，点击后会告诉你开启方法（需要发个短信），然后按照指示操作，最终会给你一个密码，这个密码可以用于在代码中当作邮箱密码
+#         # 注意!!!:163邮箱之类的不知道要不要这些操作，如果是163邮箱你可以忽略此步骤
+#         ###########################################################################
+#         smtp_server = SMTP_SSL("smtp.qq.com",
+#                                        465)  # 第一个参数：smtp服务地址（你发送邮件所使用的邮箱的smtp地址，在网上可以查到，比如qq邮箱为smtp.qq.com） 第二个参数：对应smtp服务地址的端口号
+#         smtp_server.login(from_addr, password)  # 第一个参数：发送者的邮箱账号 第二个参数：对应邮箱账号的密码
+#         #################################
+#
+#         smtp_server.sendmail(from_addr, ["2581177069@qq.com", "502258389@qq.com"],
+#                              content.as_string())  # 第一个参数：发送者的邮箱账号；第二个参数是个列表类型，每个元素为一个接收者；第三个参数：邮件内容
+#         smtp_server.quit()  # 发送完成后加上这个函数调用，类似于open文件后要跟一个close文件一样
+#     except Exception as e:
+#         print(e)
 
 
 @scheduler.scheduled_job('cron', month='1-12', day='1-31', hour='00', minute='02', second='0')
 def main():
     all_topic_urls = get_article_urls()
     save_category('健康知识', '')
-    # 获取到健康知识每一页的url[[
+    # 获取到健康知识每一页的urls
     get_health_urls(domain_health)
     health = executor.submit(get_health, health_urls)
     cdc = executor.submit(get_cdc, all_topic_urls)
     wait([health, cdc], return_when=ALL_COMPLETED)
+    # now = datetime.now()
+    # now = datetime.strftime(now, "%Y-%m-%d %H:%M:%S")
+    # sendMail('{}爬取疾控中心任务运行成功！'.format(now))
 
 
 if __name__ == "__main__":
     executor = ThreadPoolExecutor(max_workers=10)
-    main()
+    # main()
     with open('log.txt', 'a', encoding='utf8') as f:
         try:
             scheduler.start()
             f.write('任务运行成功!\n')
         except Exception:
             scheduler.shutdown()
+            # sendMail('爬取疾控中心任务运行失败，请检查原因！')
             f.write('***********************任务运行失败!*****************************\n')
